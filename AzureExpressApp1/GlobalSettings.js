120a2fdc0c122da643fa99518a86fcef84e70877
Zhuoli Liang <zhuoliseattle@gmail.com>
2017-07-04 15:40:08 -0700

    Cstock page succeed on displaying MongoDB table

---
+++ AzureExpressApp1/GlobalSettings.js
@@ -0,0 +1,3 @@
+﻿'use strict';
+
+exports.defaultDB = 'mongodb://stockdbzhuoli:QrLUUzcspLOK2pjdEvVlevms5zCfvhQlChWOtrLVRI1r5HF1mKwAKwwFm296SBSWLoOPnAQ8apN8zaPPYA3inQ==@stockdbzhuoli.documents.azure.com:10255/stockdb?ssl=true&replicaSet=globaldb'
--- AzureExpressApp1/routes/cstock.js
+++ AzureExpressApp1/routes/cstock.js
@@ -1,34 +1,26 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var settings = require('../GlobalSettings')
+var mongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    var mongoClient = require('mongodb').MongoClient
-    var url = "mongodb://stockdbzhuoli:QrLUUzcspLOK2pjdEvVlevms5zCfvhQlChWOtrLVRI1r5HF1mKwAKwwFm296SBSWLoOPnAQ8apN8zaPPYA3inQ==@stockdbzhuoli.documents.azure.com:10255/stockdb?ssl=true&replicaSet=globaldb";
-
-    var cstockArr = []
-    mongoClient.connect(url, function(err, db) {
+    mongoClient.connect(process.env.MONGODB_URI || settings.defaultDB, function (err, db) {
         if (err) throw err
-        //create collection
-        //db.createCollection("usstockcompany", function (err, collection) {
-        //    if (err) throw err;
-
-        //    console.log("Created testCollection");
-        //    console.log(collection);
-        //});
-
-        var result = db.collection('usstockcompany').find();
-        result.each(function (err, doc) {
-            console.log('Exists: ', doc.length > 0);
-            console.log("Found one item!");
-            console.log(doc);
-            console.log(err);
-            cstockArr.push(doc); //Push result onto results_array
-        })
-        db.close();
+        var all = db.collection('chinesestockmarket').find();
+        all.toArray(function (err, results) {
+            if (err != null) {
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                });
+            }
+            else {
+                res.render('cstock', { title: 'Cstock', stocks: results });
+            }
+        });
     })
-    res.render('cstock', { title: 'Cstock', stocks: cstockArr });
 });
 
 module.exports = router;
