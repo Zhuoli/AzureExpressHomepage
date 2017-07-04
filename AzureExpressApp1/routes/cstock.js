2c5ced87a42aca83303617c781af4349a6f43490
zhuoli liang <zhuoliseattle@gmail.com>
2017-07-03 23:55:42 -0700

    cstock have table in pug ready

--- AzureExpressApp1/routes/cstock.js
+++ AzureExpressApp1/routes/cstock.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('cstock', { title: 'Cstock' });
+    res.render('cstock', { title: 'Cstock', stocks : [] });
 });
 
 module.exports = router;
