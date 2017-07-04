c48536053744e327a9254e29e87601682a2eb3f1
zhuoli liang <zhuoliseattle@gmail.com>
2017-07-04 00:24:15 -0700

    Add mongodb package to package.json by running: npm install mongodb --save

--- AzureExpressApp1/routes/cstock.js
+++ AzureExpressApp1/routes/cstock.js
@@ -3,7 +3,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
+    var mongoClient = require('mongodb').MongoClient
+
+    mongoClient.connect("mongodb://stockdbzhuoli:QrLUUzcspLOK2pjdEvVlevms5zCfvhQlChWOtrLVRI1r5HF1mKwAKwwFm296SBSWLoOPnAQ8apN8zaPPYA3inQ==@stockdbzhuoli.documents.azure.com:10255/?ssl=true", function(err, db) {
+        if (err) throw err
+        db.collection('stockcompany').find().toArray(function(err, result) {
+            if (err) throw err
+
+            console.log(result)
+        })
+    })
     res.render('cstock', { title: 'Cstock', stocks : [] });
 });
 
