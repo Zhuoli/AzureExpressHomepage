87c9bc85597bfe9a9c65fddf95c01d73aec712c9
Zhuoli Liang <zhuoliseattle@gmail.com>
2017-07-10 18:24:32 -0700

    Basic version of login with fixed users works

--- AzureExpressApp1/app.js
+++ AzureExpressApp1/app.js
@@ -12,6 +12,47 @@ var users = require('./routes/users');
 var em = require('./routes/em');
 var cstock = require('./routes/cstock');
 var ustock = require('./routes/ustock');
+var login = require('./routes/login');
+
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+    function (username, password, cb) {
+        db.users.findByUsername(username, function (err, user) {
+            if (err) { return cb(err); }
+            if (!user) { return cb(null, false); }
+            if (user.password != password) { return cb(null, false); }
+            return cb(null, user);
+        });
+    }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    db.users.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
+});
+
 
 var app = express();
 
@@ -22,6 +63,7 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -32,6 +74,30 @@ app.use('/users', users);
 app.use('/em', em);
 app.use('/cstock', cstock);
 app.use('/ustock', ustock);
+app.use('/login', login);
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.post('/login',
+    passport.authenticate('local'),
+    function (req, res) {
+        res.redirect('/users/' + req.user.username);
+    });
+
+app.get('/logout',
+    function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
