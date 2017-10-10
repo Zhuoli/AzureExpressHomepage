ad277218fad516a5239e45db6eda0c0a9a921dff
Zhuoli <zhuoliseattle@gmail.com>
2017-10-10 01:42:13 -0700

    layout done

--- StockPortal/client/main.js
+++ StockPortal/client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import './cstock.html';
+//import './cstock.html';
 
 
 Router.configure({
@@ -28,9 +28,6 @@ Router.route('/ustock', function () {
 });
 
 Router.route('/cstock', function () {
-  this.render('navbar', {
-    to:"navbar"
-  });
   this.render('cstock', {
     to:"main"
   });
