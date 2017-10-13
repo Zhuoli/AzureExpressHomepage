1c2091a1f32abb0d61f7dee2ded1620d1ef7d900
Zhuoli Liang <zhuoliseattle@gmail.com>
2017-10-13 16:41:52 -0700

    update cstock path

--- StockPortal/client/stylesheets/main.js
+++ StockPortal/client/stylesheets/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import '../main.html';
-//import './cstock.html';
+import '../cstock.html';
 
 
 Router.configure({
