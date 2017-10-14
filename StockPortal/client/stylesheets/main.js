e572a4b1431d5f6e34a5ec04ef6a32e880e8f910
Zhuoli <zhuoliseattle@gmail.com>
2017-10-14 02:02:44 -0700

    database shows on page

--- StockPortal/client/stylesheets/main.js
+++ StockPortal/client/stylesheets/main.js
@@ -4,6 +4,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import '../main.html';
 import '../cstock.html';
 
+// this collection stores all the documents 
+this.cstockDB = new Mongo.Collection("chinesestockmarket");
 
 Router.configure({
   layoutTemplate: 'ApplicationLayout'
@@ -62,46 +64,41 @@ Accounts.ui.config({
 passwordSignupFields: "USERNAME_AND_EMAIL"
 });
 
+var samples = [
+  {
+    _id:"1",
+    companyname:"jd",
+    changePercent:"7",
+    currentPrice:"5",
+    price2EarningRatio:2.5,
+    price2BookRatio:1.5,
+    concepts:2.1,
+    dateFirstIPO:"9-13-2008",
+    lastUpdatedTime:"11-21-2017"
+  },
+    {
+    _id:"6",
+    companyname:"apple",
+    changePercent:"7",
+    currentPrice:"5",
+    price2EarningRatio:2.5,
+    price2BookRatio:1.5,
+    concepts:2.1,
+    dateFirstIPO:"9-13-2008",
+    lastUpdatedTime:"11-21-2017"
+  }
+]
 /// 
+Template.cstock.helpers({
+   stocks: function(){
+    var cstocks = cstockDB.find();
+    console.log("Chiense stock size: " + cstocks.count());
+    return cstocks;
+    //return samples;
+    } 
+})
 
 
-Template.images.helpers({
-images:function(){
-  if (Session.get("userFilter")){// they set a filter!
-    return Images.find({createdBy:Session.get("userFilter")}, {sort:{createdOn: -1, rating:-1}});         
-  }
-  else {
-    return Images.find({}, {sort:{createdOn: -1, rating:-1}, limit:Session.get("imageLimit")});         
-  }
-},
-filtering_images:function(){
-  if (Session.get("userFilter")){// they set a filter!
-    return true;
-  } 
-  else {
-    return false;
-  }
-},
-getFilterUser:function(){
-  if (Session.get("userFilter")){// they set a filter!
-    var user = Meteor.users.findOne(
-      {_id:Session.get("userFilter")});
-    return user.username;
-  } 
-  else {
-    return false;
-  }
-},
-getUser:function(user_id){
-  var user = Meteor.users.findOne({_id:user_id});
-  if (user){
-    return user.username;
-  }
-  else {
-    return "anon";
-  }
-}
-});
 
 Template.body.helpers({username:function(){
 if (Meteor.user()){
@@ -114,58 +111,4 @@ else {
 }
 });
 
-Template.images.events({
-'click .js-image':function(event){
-    $(event.target).css("width", "50px");
-}, 
-'click .js-del-image':function(event){
-   var image_id = this._id;
-   console.log(image_id);
-   // use jquery to hide the image component
-   // then remove it at the end of the animation
-   $("#"+image_id).hide('slow', function(){
-    Images.remove({"_id":image_id});
-   })  
-}, 
-'click .js-rate-image':function(event){
-  var rating = $(event.currentTarget).data("userrating");
-  console.log(rating);
-  var image_id = this.data_id;
-  console.log(image_id);
-
-  Images.update({_id:image_id}, 
-                {$set: {rating:rating}});
-}, 
-'click .js-show-image-form':function(event){
-  $("#image_add_form").modal('show');
-}, 
-'click .js-set-image-filter':function(event){
-    Session.set("userFilter", this.createdBy);
-}, 
-'click .js-unset-image-filter':function(event){
-    Session.set("userFilter", undefined);
-}, 
-});
-
-
-
-Template.image_add_form.events({
-'submit .js-add-image':function(event){
-  var img_src, img_alt;
-
-    img_src = event.target.img_src.value;
-    img_alt = event.target.img_alt.value;
-    console.log("src: "+img_src+" alt:"+img_alt);
-    if (Meteor.user()){
-      Images.insert({
-        img_src:img_src, 
-        img_alt:img_alt, 
-        createdOn:new Date(),
-        createdBy:Meteor.user()._id
-      });
-  }
-    $("#image_add_form").modal('hide');
- return false;
-}
-});
 
--- StockPortal/server/main.js
+++ StockPortal/server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 
+// this collection stores all the documents 
+this.cstockDB = new Mongo.Collection("chinesestockmarket");
+// this.cstockDB.insert(
+// {
+// 	    _id:"1",
+//     companyname:"jd",
+//     changePercent:"7",
+//     currentPrice:"5",
+//     price2EarningRatio:2.5,
+//     price2BookRatio:1.5,
+//     concepts:2.1,
+//     dateFirstIPO:"9-13-2008",
+//     lastUpdatedTime:"11-21-2017"
+//   }
+//  )
+    var cstocks = this.cstockDB.find();
+    console.log("Chiense stock size: " + cstocks.count());
 Meteor.startup(() => {
   // code to run on server at startup
+  process.env.MONGO_URL='mongodb://stockmarketdbzhuoli:FMB5jJ6pGLwDlTzpJxCM6sD2gE7Y1UAFoQ7tWVpppTz5W9AXe0NnerY5GaRkfPMYZIa7xopJUeMaduHsOwVm6Q==@stockmarketdbzhuoli.documents.azure.com:10255/stockdb?ssl=true&replicaSet=globaldb'
+
 });
