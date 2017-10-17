8404cf8cc1aeba850fe95ebcd010eb4b90e37e7b
Zhuoli Liang <zhuoliseattle@gmail.com>
2017-10-16 18:17:14 -0700

    Update navbar to active and deactive on click element

--- StockPortal/client/stylesheets/main.js
+++ StockPortal/client/stylesheets/main.js
@@ -20,11 +20,21 @@ Router.route('/', function () {
   });
 });
 
+Router.route('/em', function () {
+  this.render('navbar', {
+    to:"navbar"
+  });
+  this.render('em', {
+    to:"main"
+  });
+});
+
+
 Router.route('/ustock', function () {
   this.render('navbar', {
     to:"navbar"
   });
-  this.render('images', {
+  this.render('ustock', {
     to:"main"
   });
 });
@@ -38,28 +48,6 @@ Router.route('/cstock', function () {
   });
 });
      
-
-
-/// infiniscroll
-
-Session.set("imageLimit", 8);
-lastScrollTop = 0; 
-$(window).scroll(function(event){
-// test if we are near the bottom of the window
-if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-  // where are we in the page? 
-  var scrollTop = $(this).scrollTop();
-  // test if we are going down
-  if (scrollTop > lastScrollTop){
-    // yes we are heading down...
-   Session.set("imageLimit", Session.get("imageLimit") + 4);
-  }
-
-  lastScrollTop = scrollTop;
-}
-    
-})
-
 
 /// accounts config
 
@@ -114,4 +102,12 @@ else {
 }
 });
 
+
+/// Events
+Template.navbar.Events({
+  'click li a'(event){
+    $('li').removeClass('active');
+    $('this').addClass('active');
+  }
+})
 
